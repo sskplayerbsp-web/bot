9482dc86a82d473ff32bb80cff0b8da5680699fa
sskplayerbsp-web <sskplayer.bsp@gmail.com>
2026-06-03 21:59:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,4 +52,3 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Bot is Alive!'));
 app.listen(PORT, () => console.log(`[Web Server] Listening on port ${PORT}`));
-
